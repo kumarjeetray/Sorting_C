d3590a43a997c39149376815c6494410575e7473
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2019-08-13 22:48:58 +0530

    Add files via upload

---
+++ Bubble_back.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+ 
+void main()
+{
+	int a[50],n,i,j,temp;
+	printf("Enter the size of array: ");
+	scanf("%d",&n);
+	
+	
+	for(i=0;i<n;++i)
+    {
+        printf("\nEnter the array elements: ");
+		scanf("%d",&a[i]);
+    }
+	for(i=1;i<n;i++)
+    {
+		for(j=0;j<(n-i);j++)
+        {
+			if(a[j]<a[j+1])
+			{
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+        }
+    }
+			
+	printf("\nArray after sorting: ");
+	for(i=0;i<n;++i)
+		printf("%d ",a[i]);
+ 
+}
